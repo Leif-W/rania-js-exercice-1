3d71faf549f7f9e405c9c08f69c803384c96d77a
rania_codes <62594732+rania-codes@users.noreply.github.com>
2020-09-14 12:01:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function somme() {
-  document.querySelectorAll("form").forEach((form) => {
-    var som = 0;
-    var num1 = document.getElementById("num1");
-    var num2 = document.getElementById("num2");
-    som += (parseInt(num1.value) + 2 * parseInt(num2.value)) / 3;
-    var output = document.getElementById("sum");
-    output.innerHTML = som;
-  });
-  console.log(som);
+  var inputs = document.getElementsByClassName("note-devoir");
+  var sum = 0;
+
+  var table = document.getElementById("tbl");
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    sum = 0;
+    //iterate through rows
+    //rows would be accessed using the "row" variable assigned in the for loop
+    for (var j = 1, col; (col = row.cells[j]); j++) {
+      //iterate through columns
+      //columns would be accessed using the "col" variable assigned in the for loop
+      if (col.classList.contains("col-sum")) {
+        col.children[0].innerText = sum;
+        break;
+      }
+
+      if (col.getElementsByClassName("num")[0]) {
+        sum += parseFloat(col.getElementsByClassName("num")[0].value);
+      }
+    }
+  }
+
+  sum = 0;
 }
